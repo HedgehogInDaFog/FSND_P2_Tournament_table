863372228d04ee031b93867260103ff13f8de861
vvorotnikov <v.s.vorotnikov@gmail.com>
2016-04-08 15:43:23 +0300

    First try on DB Schema

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,23 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
 
+CREATE TABLE players (
+	pid		integer PRIMARY KEY nextval('serial'),
+	name	text
+);
+
+CREATE TABLE results (
+	gid			integer PRIMARY KEY nextval('serial'),
+	tournament	integer,
+	player1		integer,
+	player2		integer,
+	round		integer,
+	result		varchar(5)
+);
+
+CREATE TABLE tournament_members (
+	tid		integer,
+	pid		integer
+);
